585a229023240cdc11871ebd41c5bf29d6d305f8
Cristian Descosido <cristiandescosido@gmail.com>
2021-08-16 15:24:23 -0300

    asdasd

---
+++ README.md
@@ -0,0 +1,1 @@
+Evaluaciòn Unidad Nº5
---
+++ Script/script.js
@@ -0,0 +1,11 @@
+var isRecruiter = confirm('¿Eres un reclutador?');
+
+if (isRecruiter == true) {
+var currentNewRecruiterName = prompt('¿Cuàl es su nombre?');
+var currentNewRecruiterFirm = prompt('¿Para que empresa trabajas?');
+var currentNewRecruiterMail = prompt('¿Cuàl es su e-mail?');
+} else {
+var currentNewUserName = prompt('¿Cuàl es su nombre?');
+var currentNewUserOccupation = prompt('¿A que te dedicas?');
+var currentNewUserContact = prompt('¿Còmo te puedo contactar?');
+}
